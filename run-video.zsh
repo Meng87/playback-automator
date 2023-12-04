bde4f677715718c8c6a7df8db375c79da3a3523a
Meng87 <79801878+Meng87@users.noreply.github.com>
2023-12-04 02:28:25 -0500

    Include cmds to run videos on tv

--- run-video.zsh
+++ run-video.zsh
@@ -11,16 +11,23 @@ netflix_username= # TODO
 netflix_password= # TODO
 netflix_url= # TODO
 netflix_time= # TODO
-temp_folder= # TODO # where temporary files will be generated
+temp_folder= # TODO
 
 function play_video {
     package=${1:?"Specify package name."}
     url=${2:?"Specify url."}
     playback_time=${3:?"Specify playback time."}
+    is_tv=${4:?"Specify whether device is Android TV"}
 
     adb shell pm clear $package # Clear app data
 
-    adb shell am start -a android.intent.action.VIEW -d $url
+    if [[ $is_tv -eq 1 ]]
+    then
+        adb shell am start -a android.intent.action.VIEW -d $url $package
+    else
+        adb shell am start -a android.intent.action.VIEW -d $url
+    fi
+
     sleep $playback_time
 
     adb shell pm clear $package # Clear app data
@@ -38,8 +45,8 @@ function play_netflix {
 
     # Open Netflix app
     adb shell am start -n com.netflix.mediaclient/com.netflix.mediaclient.acquisition.screens.signupContainer.SignupNativeActivity
-    
-    # Click on sign in button
+
+    # Navigate to log in page
     adb shell uiautomator dump
     adb pull /sdcard/window_dump.xml $tmp_folder/mainpage.xml
     python3 $tmp_folder/parse.py mainpage.xml "SIGN IN" | xargs -I {} adb shell input tap {}
@@ -65,6 +72,51 @@ function play_netflix {
     rm $tmp_folder/login.xml
 }
 
-play_video com.google.android.youtube $youtube_url $youtube_time
-play_video com.vimeo.android.videoapp $vimeo_url $vimeo_time
-play_netflix $netflix_username $netflix_password $netflix_url $netflix_time $temp_folder
+function play_netflix_tv {
+    username=${1:?"Specify username."}
+    pw=${2:?"Specify password."}
+    url=${3:?"Specify url."}
+    playback_time=${4:?"Specify playback time."}
+
+    # Clear app data
+    adb shell pm clear com.netflix.ninja
+
+    # Open Netflix app
+    adb shell am start -n com.netflix.ninja/.MainActivity
+    sleep 16
+
+    # Navigate to log in page
+    adb shell input keyevent KEYCODE_DPAD_LEFT
+    adb shell input press
+    sleep 1
+    adb shell input keyevent KEYCODE_DPAD_RIGHT
+    adb shell input press
+    sleep 1
+
+    # Log in
+    adb shell input text $username
+    adb shell input press
+    sleep 1
+    adb shell input text $pw
+    adb shell input text x
+    adb shell input keyevent KEYCODE_DEL
+    adb shell input keyevent KEYCODE_DPAD_DOWN
+    adb shell input press
+    sleep 15
+
+    # Play video
+    adb shell am start -c android.intent.category.LEANBACK_LAUNCHER -a android.intent.action.VIEW -d $url -e source 30 -n com.netflix.ninja/.MainActivity
+    sleep $playback_time
+
+    # Clear app data
+    adb shell pm clear com.netflix.ninja
+}
+
+# Phone
+# play_video com.google.android.youtube $youtube_url $youtube_time 0
+# play_video com.vimeo.android.videoapp $vimeo_url $vimeo_time 0
+# play_netflix $netflix_username $netflix_password $netflix_url $netflix_time $temp_folder
+
+# TV
+# play_video com.google.android.youtube.tv $youtube_url $youtube_time 1
+play_netflix_tv $netflix_username $netflix_password $netflix_url $netflix_time
